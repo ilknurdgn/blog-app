386ccc38b3740b091457750000c37c35ed6a3731
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-13 07:45:05 +0300

    feat(pages): create home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css';
+import Header from '../../components/header/Header';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Header />
+      homepage
+    </div>
+  );
+};
+
+export default Home;
