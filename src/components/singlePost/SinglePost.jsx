37d3f43e3c7e3b85a19b2b605d7f5de2bbaad3c8
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-14 11:11:05 +0300

    feat(components): create SinglePost component

---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import './singlePost.css';
+
+const SinglePost = () => {
+  return (
+    <div className='singlePost'>
+      <div className='singlePostWrapper'>
+        <img
+          src='https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+          alt=''
+          className='singlePostImg'
+        />
+        <h1 className='singlePostTitle'>
+          Lorem ipsum dolor sit amet
+          <div className='singlePostEdit'>
+            <i className='singlePostIcon fa-regular fa-pen-to-square'></i>
+            <i className='singlePostIcon fa-regular fa-trash-can'></i>
+          </div>
+        </h1>
+        <div className='singlePostInfo'>
+          <span className='singlePostAuthor'>
+            Author: <b>Safak</b>
+          </span>
+          <span className='singlePostDate'>1 hour ago</span>
+        </div>
+        <p className='singlePostDesc'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
+          rerum natus laudantium facere cum necessitatibus hic magni. Asperiores
+          sunt laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis rerum natus laudantium facere cum
+          necessitatibus hic magni. Asperiores sunt laborum provident accusamus
+          a, et quae consequuntur mollitia reiciendis at perspiciatis.Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis rerum
+          natus laudantium facere cum necessitatibus hic magni. Asperiores sunt
+          laborum provident accusamus a, et quae consequuntur mollitia
+          reiciendis at perspiciatis.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePost;
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './single.css';
 import Sidebar from '../../components/sidebar/Sidebar.jsx';
+import SinglePost from '../../components/singlePost/SinglePost.jsx';
 
 const Single = () => {
   return (
     <div className='single'>
-      {/* post */}
+      <SinglePost />
       <Sidebar />
     </div>
   );
