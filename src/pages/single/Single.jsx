44e1e180cb22f2c2d2f7b5604bae723ea5ecd3bb
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-14 10:20:53 +0300

    feat(pages): create single page

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './single.css';
+import Sidebar from '../../components/sidebar/Sidebar.jsx';
+
+const Single = () => {
+  return (
+    <div className='single'>
+      {/* post */}
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Single;
