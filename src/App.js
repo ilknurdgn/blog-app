16fd253a05077a8b9155d011f66fda9c9f21e927
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-14 14:03:01 +0300

    feat(pages): create Write page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
-import Single from './pages/single/Single';
+import Write from './pages/write/Write';
 
 function App() {
   return (
     <>
       <Topbar />
       {/* <Home /> */}
-      <Single />
+      <Write />
     </>
   );
 }
