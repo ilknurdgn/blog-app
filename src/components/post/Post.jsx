0ebe80b75043594d944ca8015fab60aeab82f353
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-22 14:59:58 +0300

    feat(Post.jsx): insert all post information

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,46 +2,26 @@ import React from 'react';
 import './post.css';
 import { Link } from 'react-router-dom';
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
     <div className='post'>
-      <img
-        className='postImg'
-        src='http://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto-compress&cs=tinysrgb&dqr=2&w=500'
-        alt=''
-      />
+      {post.photo && <img className='postImg' src={post.photo} alt='' />}
+
       <div className='postInfo'>
         <div className='postCats'>
-          <span className='postCat'>
-            <Link className='link' to='/posts?cat=music'>
-              Music
-            </Link>
-          </span>
-          <span className='postCat'>
-            <Link className='link' to='/posts?cat=life'>
-              Life
-            </Link>
-          </span>
+          {post.categories.map((c) => (
+            <span className='postCat'>{c.name}</span>
+          ))}
         </div>
-        <Link className='link postTitle' to='/post/abc'>
-          Lorem ipsum dolor sit amet
+        <Link className='link postTitle' to={`/post/${post._idgit}`}>
+          {post.title}
         </Link>
         <hr />
-        <span className='postDate'>1 hour ago</span>
+        <span className='postDate'>
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className='postDesc'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est maxime
-        distinctio itaque autem dignissimos corporis asperiores suscipit
-        tenetur, dolorum corrupti amet libero architecto molestias voluptatibus
-        nulla necessitatibus deleniti ea delectus! Lorem ipsum dolor sit, amet
-        consectetur adipisicing elit. Est maxime distinctio itaque autem
-        dignissimos corporis asperiores suscipit tenetur, dolorum corrupti amet
-        libero architecto molestias voluptatibus nulla necessitatibus deleniti
-        ea delectus! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Est maxime distinctio itaque autem dignissimos corporis asperiores
-        suscipit tenetur, dolorum corrupti amet libero architecto molestias
-        voluptatibus nulla necessitatibus deleniti ea delectus!
-      </p>
+      <p className='postDesc'>{post.desc}</p>
     </div>
   );
 };
