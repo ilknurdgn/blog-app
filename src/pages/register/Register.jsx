fadaa68a6eba86e255024230a80a8b013476fef2
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-31 07:03:26 +0300

    feat(register): make register system functional

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './register.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    setError(false);
+    e.preventDefault();
+    try {
+      const res = await axios.post('/auth/register', {
+        username,
+        email,
+        password,
+      });
+
+      res.data && window.location.replace('/login');
+    } catch (error) {
+      setError(true);
+    }
+  };
   return (
     <div className='register'>
       <span className='registerTitle'>REGISTER</span>
 
-      <form className='registerForm'>
+      <form className='registerForm' onSubmit={handleSubmit}>
         <label htmlFor='usernameInput'>Username</label>
         <input
           type='text'
           placeholder='Enter your username...'
           id='usernameInput'
           className='registerInput'
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <label htmlFor='emailInput'>Email</label>
@@ -22,6 +44,7 @@ const Register = () => {
           placeholder='Enter your email...'
           id='emailInput'
           className='registerInput'
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <label htmlFor='passwordInput'>Password</label>
@@ -30,12 +53,18 @@ const Register = () => {
           placeholder='Enter your password...'
           id='passwordInput'
           className='registerInput'
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button className='registerButton'>REGISTER</button>
       </form>
       <Link className='link' to='/login'>
         <button className='registerLoginButton'>LOGIN</button>
+        {error && (
+          <span style={{ color: 'red', marginTop: '10px' }}>
+            Something went wrong!
+          </span>
+        )}
       </Link>
     </div>
   );
